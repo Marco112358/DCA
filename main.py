fbda128af7f965aa2bedc9ae4bb9f68dee8ff41e
Marco112358 <marcomarco112358@gmail.com>
2022-10-02 12:42:48 -0400

    Changed to allow for transitional probabilities

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 from __future__ import division
 import numpy as np
 import pandas as pd
+import scipy.stats as sc
 
 
 # VARIABLES
-ann_ret = 0.07
+ann_ret = 0.20
 ann_ret_adj = 0.0
-ann_std = 0.19
+ann_std = 0.5
 ann_cash_ret = 0.02
 ann_cash_std = 0
-p = 12  # number of periods per year, 4 for quarter, 12 for months, etc.
-a = 30  # number of years
-n = 50000  # number of trials
+p = 365  # number of periods per year, 4 for quarter, 12 for months, etc.
+a = 1  # number of years
+n = 100  # number of trials
 st_val = 100.00
 # set amount of periods to dca over
 # dca_m_vals = range(6, 37,6)
-dca_m = 12  # number of periods to DCA in (if p = 12 and dca_m = 12, DCA in 1/12 per month for first year)
-
+dca_m = 90  # number of periods to DCA in (if p = 12 and dca_m = 12, DCA in 1/12 per month for first year)
+trans_matrix = np.asmatrix([[0.5901981, 0.4526559], [0.409819, 0.5473441]])
+ # [[uu, du],[ud, dd]]
 
 def lognrm_mean_calc(per_ret, per_std):
     ln_nrm_mean = np.log(((1 + per_ret) ** 2) / np.sqrt(per_std ** 2 + ((1 + per_ret) ** 2)))
@@ -52,6 +54,7 @@ def period_data(p, a, ann_ret, ann_ret_adj, ann_std, ann_cash_ret):
     # per_ret_cash = ann_cash_ret / p
     return m, per_ret, per_std, per_ret_cash
 
+
 def cum_fv(annuity, rands, m, st_val, per_ret_cash):
     # Cash compounds per period... excess interest over starting value stays in cash account....
     final = np.zeros(m)
@@ -81,6 +84,105 @@ def mc(n, m, mean, std, dca_annuity, lump_annuity, st_val, per_ret_cash, rand_fu
     for i in range(n):
         # THIS CREATES RANDOM RETURNS
         rands = rand_function(mean, std, m)
+        # calculate values at each period
+        dca_fv = cum_fv(dca_annuity, rands, m, st_val, per_ret_cash)
+        lump_fv = cum_fv(lump_annuity, rands, m, st_val, per_ret_cash)
+        # return final value
+        final_dca_vals.append(dca_fv[m - 1])
+        final_lump_vals.append(lump_fv[m - 1])
+    return final_dca_vals, final_lump_vals
+
+
+def stdnorm_rndm_var():
+    # standard normal variable that should change every call
+    x = sc.norm.rvs()
+    return x
+
+
+def lognorm_rndm_var(mean=0, std=1):
+    # FAIL
+    # ex = np.exp(mean + 0.5 * std ** 2)
+    # varx = np.exp(2 * mean + std ** 2) * (np.exp(std ** 2) - 1)
+    # varx = np.exp(2 * mean + 2 * std ** 2) - ex ** 2
+    # logmean1 = np.log((mean ** 2) / ((mean ** 2 + std ** 2) ** 0.5))
+    # logstd1 = np.log(1 + (std ** 2) / (mean ** 2))
+    logstd = (np.log(1 + (std / (1 + mean)) ** 2)) ** 0.5
+    # logmean = np.log(1 + mean) - (logstd ** 2) / 2
+    x = sc.lognorm.rvs(logstd)
+    return x
+
+
+def prc_new(prc_prev=0, mean=0.0, std=0.0, step=1, days=1, dist="norm"):
+    # Standard Normal Distribution used
+    # Create a new wealth based on Brownian Motion off of Previous Wealth
+    mean_fin = mean * days / 365
+    std_fin = std / np.sqrt(365 / days)
+    if dist == "norm":
+        price_new = prc_prev * np.exp((mean_fin - 0.5 * (std_fin ** 2)) * step + std_fin * (step ** 2)
+                                      * stdnorm_rndm_var())
+#    elif dist == "log":
+#        price_new = prc_prev * np.exp((mean_fin - 0.5 * (std_fin ** 2)) * step + std_fin * (step ** 2) *
+#                                      (lognorm_rndm_var(mean, std) - 1))
+    return price_new
+
+
+def prc_new_with_transprobs(prc_prev=0, mean=0.0, std=0.0, step=1, days=1, p=365, price_prev2=0,
+                            transmatrix=np.matrix([[0, 0], [0, 0]])):
+    # Standard Normal Distribution used
+    # Create a new wealth based on Brownian Motion off of Previous Wealth
+    mean_fin = mean * days / p
+    std_fin = std / np.sqrt(p / days)
+    if prc_prev < price_prev2:
+        price_new = transmatrix[0, 1] * (prc_prev * np.exp((mean_fin - 0.5 * (std_fin ** 2)) * step +
+                                                           std_fin * (step ** 2) * abs(stdnorm_rndm_var()))) + \
+                    transmatrix[1, 1] * (prc_prev * np.exp((mean_fin - 0.5 * (std_fin ** 2)) * step +
+                                                           std_fin * (step ** 2) * -abs(stdnorm_rndm_var())))
+    elif prc_prev == price_prev2:
+        price_new = (prc_prev * np.exp((mean_fin - 0.5 * (std_fin ** 2)) * step +
+                                                           std_fin * (step ** 2) * stdnorm_rndm_var()))
+    else:
+        price_new = transmatrix[0, 0] * (prc_prev * np.exp((mean_fin - 0.5 * (std_fin ** 2)) * step +
+                                                           std_fin * (step ** 2) * abs(stdnorm_rndm_var()))) + \
+                    transmatrix[1, 0] * (prc_prev * np.exp((mean_fin - 0.5 * (std_fin ** 2)) * step +
+                                                           std_fin * (step ** 2) * -abs(stdnorm_rndm_var())))
+#    elif dist == "log":
+#        price_new = prc_prev * np.exp((mean_fin - 0.5 * (std_fin ** 2)) * step + std_fin * (step ** 2) *
+#                                      (lognorm_rndm_var(mean, std) - 1))
+    return price_new
+
+
+def rnd_prc_tbl(prc_st=None, mean=0, std=0, days=1, no_trials=1, no_periods=1, p=365,
+                transmatrix=np.matrix([[0, 0], [0, 0]])):
+    fin = pd.DataFrame(index=np.arange(0, no_trials), columns=np.arange(0, int(no_periods / days) + 1))
+    for n in np.arange(0, no_trials):
+        fin.iloc[n, 0] = prc_st
+        for t in np.arange(1, int(no_periods / days) + 1):
+            if t == 1 or t == 2:
+                fin.iloc[n, t] = prc_new_with_transprobs(prc_st, mean, std, 1, days, p, prc_st, transmatrix)
+            else:
+                fin.iloc[n, t] = prc_new_with_transprobs(fin.iloc[n, t - 1], mean, std, 1, days,  p,
+                                                         fin.iloc[n, t - 2], transmatrix)
+    return fin
+
+
+def return_tbl(tbl=None):
+    out = pd.DataFrame(index=tbl.index, columns=tbl.columns)
+    for i, col in enumerate(tbl):
+        if i == 0:
+            out.loc[:,0] = 0.0
+        else:
+            out.loc[:, i] = tbl.loc[:, i] / tbl.loc[:, i-1] - 1
+    return out
+
+
+def mc2(n, m, dca_annuity, lump_annuity, st_val, per_ret_cash, rand_rtns):
+    # empty arrays for final values
+    final_dca_vals = []
+    final_lump_vals = []
+    # run MC and append to tables
+    for tr in range(n):
+        # THIS CREATES RANDOM RETURNS
+        rands = rand_rtns.loc[tr, :]
         # calculate values at each period
         dca_fv = cum_fv(dca_annuity, rands, m, st_val, per_ret_cash)
         lump_fv = cum_fv(lump_annuity, rands, m, st_val, per_ret_cash)
@@ -130,8 +232,14 @@ ln_nrm_mean = lognrm_mean_calc(per_ret, per_std)
 ln_nrm_std = lognrm_std_calc(per_ret, per_std)
 lump_annuity = lump_vals(m, st_val)
 dca_annuity = dca_vals(st_val, dca_m, m)
-final_dca_vals, final_lump_vals = mc(n, m, ln_nrm_mean, ln_nrm_std, dca_annuity, lump_annuity, st_val, per_ret_cash,
-                                     lognormal_rands)
+
+rnd_prcs = rnd_prc_tbl(st_val, ann_ret, ann_std, 1, n, m,  p, trans_matrix)
+rand_rtns = return_tbl(rnd_prcs)
+
+final_dca_vals, final_lump_vals = mc2(n, m, dca_annuity, lump_annuity, st_val, per_ret_cash, rand_rtns)
+
+#final_dca_vals, final_lump_vals = mc(n, m, ln_nrm_mean, ln_nrm_std, dca_annuity, lump_annuity, st_val, per_ret_cash,
+#                                     lognormal_rands)
 summary_table = summary(final_dca_vals, final_lump_vals)
 print(summary_table)
 
